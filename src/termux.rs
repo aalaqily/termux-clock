e02b8278aecdf1e45d2801fe06ccaab3b434659e
Ahmad Asaad <ahmadasaadh@gmail.com>
2024-10-19 22:17:20 +0300

    feat: set alarms in termux

--- src/termux.rs
+++ src/termux.rs
@@ -51,6 +51,54 @@ impl Frontend for Termux {
         message: Option<String>,
         vibrate: Option<bool>,
     ) {
-        todo!();
+        let mut args: Vec<String> = vec!["-c".to_string(), String::new()];
+
+        match message {
+            Some(m) => &args[1].push_str(
+                format!(
+                    "echo \"termux-notification --title 'Termux Alarm' --content '{}' ",
+                    m
+                )
+                .as_str(),
+            ),
+            None => &args[1].push_str("echo \"termux-notification --title 'Termux Alarm' "),
+        };
+
+        match vibrate {
+            Some(v) => {
+                if v {
+                    &args[1].push_str("&& termux-vibrate");
+                }
+            }
+            None => (),
+        };
+
+        match (hour, minute) {
+            (Some(h), Some(m)) => &args[1].push_str(format!("\" | at {}:{}", h, m).as_str()),
+            (Some(h), None) => &args[1].push_str(format!("\" | at {}:00", h).as_str()),
+            (None, Some(m)) => &args[1].push_str(format!("\" | at 00:{}", m).as_str()),
+            (None, None) => &args[1].push_str(format!("\" | at 00:00").as_str()),
+        };
+
+        #[cfg(debug_assertions)]
+        dbg!(&args);
+
+        #[cfg(debug_assertions)]
+        dbg!(&args);
+
+        let mut command = Command::new("sh")
+            .args(args)
+            .output()
+            .expect("Unable to set timer");
+
+        #[cfg(debug_assertions)]
+        {
+            let status = command.status;
+            let stdout = String::from_utf8(command.stdout).unwrap();
+            let stderr = String::from_utf8(command.stderr).unwrap();
+            dbg!(status);
+            dbg!(stdout);
+            dbg!(stderr);
+        }
     }
 }
