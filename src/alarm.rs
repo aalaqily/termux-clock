06b36fe028c33ab17dc8e277bf6388fb5771dff8
Ahmad Asaad <ahmadasaadh@gmail.com>
2024-10-28 10:49:51 +0200

    test: add tests for every function

--- src/alarm.rs
+++ src/alarm.rs
@@ -43,3 +43,53 @@ impl Alarm {
 	self
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+	let left = Alarm {
+	    hour: 0,
+	    minutes: 0,
+	    days: None,
+	    message: None,
+	    vibrate: false,
+	};
+
+	let right = Alarm::new();
+
+	assert_eq!(left, right);
+    }
+
+    #[test]
+    fn test_hour() {
+	let alarm = Alarm::new().hour(6);
+	assert_eq!(alarm.hour, 6);
+    }
+
+    #[test]
+    fn test_minutes() {
+	let alarm = Alarm::new().minutes(30);
+	assert_eq!(alarm.minutes, 30);
+    }
+
+    #[test]
+    fn test_days() {
+	let alarm = Alarm::new().days(vec![1, 2, 3]);
+	assert_eq!(alarm.days, Some(vec![1, 2, 3]));
+    }
+
+    #[test]
+    fn test_message() {
+	let alarm = Alarm::new().message(String::from("Wake Up!"));
+	assert_eq!(alarm.message, Some(String::from("Wake Up!")));
+    }
+
+    #[test]
+    fn test_vibrate() {
+	let alarm = Alarm::new().vibrate(true);
+	assert_eq!(alarm.vibrate, true);
+    }
+}
