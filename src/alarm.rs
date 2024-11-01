079c0fbf0134435a71ee6d654a1b16de6583726f
Ahmad Asaad <ahmadasaadh@gmail.com>
2024-11-01 05:39:52 +0200

    refactor: wrap arguments with Option<T> in alarm and timer modules

--- src/alarm.rs
+++ src/alarm.rs
@@ -1,46 +1,46 @@
 #[derive(Debug, PartialEq)]
 pub struct Alarm {
-    hour: u8,
-    minutes: u8,
-    days: Option<Vec<u8>>,
-    message: Option<String>,
-    vibrate: bool,
+    pub hour: Option<u8>,
+    pub minutes: Option<u8>,
+    pub days: Option<Vec<u8>>,
+    pub message: Option<String>,
+    pub vibrate: bool,
 }
 
 impl Alarm {
     pub fn new() -> Alarm {
-	Alarm {
-	    hour: 0,
-	    minutes: 0,
-	    days: None,
-	    message: None,
-	    vibrate: false,
-	}
+        Alarm {
+            hour: None,
+            minutes: None,
+            days: None,
+            message: None,
+            vibrate: false,
+        }
     }
 
     pub fn hour(mut self, hour: u8) -> Self {
-	self.hour = hour;
-	self
+        self.hour = Some(hour);
+        self
     }
 
     pub fn minutes(mut self, minutes: u8) -> Self {
-	self.minutes = minutes;
-	self
+        self.minutes = Some(minutes);
+        self
     }
 
     pub fn days(mut self, days: Vec<u8>) -> Self {
-	self.days = Some(days);
-	self
+        self.days = Some(days);
+        self
     }
 
     pub fn message(mut self, message: String) -> Self {
-	self.message = Some(message);
-	self
+        self.message = Some(message);
+        self
     }
 
     pub fn vibrate(mut self, vibrate: bool) -> Self {
-	self.vibrate = vibrate;
-	self
+        self.vibrate = vibrate;
+        self
     }
 }
 
@@ -50,46 +50,46 @@ mod tests {
 
     #[test]
     fn test_new() {
-	let left = Alarm {
-	    hour: 0,
-	    minutes: 0,
-	    days: None,
-	    message: None,
-	    vibrate: false,
-	};
+        let left = Alarm {
+            hour: None,
+            minutes: None,
+            days: None,
+            message: None,
+            vibrate: false,
+        };
 
-	let right = Alarm::new();
+        let right = Alarm::new();
 
-	assert_eq!(left, right);
+        assert_eq!(left, right);
     }
 
     #[test]
     fn test_hour() {
-	let alarm = Alarm::new().hour(6);
-	assert_eq!(alarm.hour, 6);
+        let alarm = Alarm::new().hour(6);
+        assert_eq!(alarm.hour, Some(6));
     }
 
     #[test]
     fn test_minutes() {
-	let alarm = Alarm::new().minutes(30);
-	assert_eq!(alarm.minutes, 30);
+        let alarm = Alarm::new().minutes(30);
+        assert_eq!(alarm.minutes, Some(30));
     }
 
     #[test]
     fn test_days() {
-	let alarm = Alarm::new().days(vec![1, 2, 3]);
-	assert_eq!(alarm.days, Some(vec![1, 2, 3]));
+        let alarm = Alarm::new().days(vec![1, 2, 3]);
+        assert_eq!(alarm.days, Some(vec![1, 2, 3]));
     }
 
     #[test]
     fn test_message() {
-	let alarm = Alarm::new().message(String::from("Wake Up!"));
-	assert_eq!(alarm.message, Some(String::from("Wake Up!")));
+        let alarm = Alarm::new().message(String::from("Wake Up!"));
+        assert_eq!(alarm.message, Some(String::from("Wake Up!")));
     }
 
     #[test]
     fn test_vibrate() {
-	let alarm = Alarm::new().vibrate(true);
-	assert_eq!(alarm.vibrate, true);
+        let alarm = Alarm::new().vibrate(true);
+        assert_eq!(alarm.vibrate, true);
     }
 }
