89248a21aa71385bea5d487cd67da1cf3f8a7984
0xAhmad <ahmadasaadh@gmail.com>
2024-11-01 13:55:16 +0000

    feat(wrappers): add termux wrapper
    - Replaces termux-api wrapper and most of termux module

---
+++ src/wrappers/termux.rs
@@ -0,0 +1,43 @@
+use crate::alarm::Alarm;
+use crate::timer::Timer;
+use std::process::Command;
+
+fn set_timer_command(timer: Timer) -> Command {
+    let mut args: Vec<String> = vec!["-c".to_string()];
+    if let Some(l) = timer.length {
+        args.push(format!("sleep {} &", l));
+    };
+    match timer.message {
+        Some(m) => {
+            &args[1].push_str(
+                format!(
+                    "& termux-notification --title \"Termux Timer ({}s)\" --content \"{}\" &",
+                    timer.length.unwrap(),
+                    m
+                )
+                .as_str(),
+            );
+        }
+        None => {
+            &args[1].push_str(format!("& termux-notification --title \"Termux Timer ({}s)\" --content \"Timer finished\" &", timer.length.unwrap()).as_str());
+        }
+    };
+
+    let mut command = Command::new("sh");
+    command.args(args);
+    command
+}
+
+fn set_alarm_string(alarm: Alarm) -> String {
+    let mut string = String::from(r#"echo "termux-notification --title 'Termux Alarm' "#);
+
+    if let Some(m) = alarm.message {
+        string.push_str(format!("--content '{}' ", m).as_str())
+    };
+
+    if alarm.vibrate {
+        string.push_str("&& termux-vibrate");
+    };
+
+    string
+}
