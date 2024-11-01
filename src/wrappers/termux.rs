d5cb217336571b84d5139cd68b85bbb411520d7d
0xAhmad <ahmadasaadh@gmail.com>
2024-11-01 14:29:13 +0000

    test: add tests for termux wrapper

--- src/wrappers/termux.rs
+++ src/wrappers/termux.rs
@@ -46,3 +46,87 @@ fn set_alarm_string(alarm: Alarm) -> String {
 
     string
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::ffi::OsStr;
+
+    #[test]
+    fn test_set_timer_command() {
+        let left_args: Vec<&OsStr> = [
+            "-c",
+            r#"sleep 60 && termux-notification --title "Termux Timer (60s)" --content "Wake up!" && termux-vibrate &"#,
+        ]
+        .iter()
+        .map(|s| OsStr::new(s))
+        .collect();
+
+        let command = set_timer_command(Timer::new().seconds(60).message(String::from("Wake up!")).vibrate(true));
+
+        let right_args = command.get_args().collect::<Vec<&OsStr>>();
+
+        assert_eq!(left_args, right_args);
+    }
+
+    #[test]
+    fn test_set_timer_command_vibrate_false() {
+        let left_args: Vec<&OsStr> = [
+            "-c",
+            r#"sleep 60 && termux-notification --title "Termux Timer (60s)" --content "Wake up!" &"#,
+        ]
+        .iter()
+        .map(|s| OsStr::new(s))
+        .collect();
+
+        let command = set_timer_command(Timer::new().seconds(60).message(String::from("Wake up!")));
+
+        let right_args = command.get_args().collect::<Vec<&OsStr>>();
+
+        assert_eq!(left_args, right_args);
+    }
+
+    #[test]
+    fn test_set_timer_command_message_none() {
+        let left_args: Vec<&OsStr> = [
+            "-c",
+            r#"sleep 60 && termux-notification --title "Termux Timer (60s)" --content "Timer finished" && termux-vibrate &"#,
+        ]
+        .iter()
+        .map(|s| OsStr::new(s))
+        .collect();
+
+        let command = set_timer_command(Timer::new().seconds(60).vibrate(true));
+
+        let right_args = command.get_args().collect::<Vec<&OsStr>>();
+
+        assert_eq!(left_args, right_args);
+    }
+
+    #[test]
+    fn test_set_alarm_string() {
+        let left = String::from("termux-notification --title 'Termux Alarm' --content 'Wake up!' && termux-vibrate");
+
+        let right = set_alarm_string(Alarm::new().message(String::from("Wake up!")).vibrate(true));
+
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn test_set_alarm_string_message_none() {
+        let left = String::from("termux-notification --title 'Termux Alarm' && termux-vibrate");
+
+        let right = set_alarm_string(Alarm::new().vibrate(true));
+
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn test_set_alarm_vibrate_false() {
+        let left = String::from("termux-notification --title 'Termux Alarm' --content 'Wake up!' ");
+
+        let right = set_alarm_string(Alarm::new().message(String::from("Wake up!")));
+
+        assert_eq!(left, right);
+    }
+}
